a2acdb27a98d6c7fe63be1ebb61568e5f78ad081
Raj Jada <Rajjada001@gmail.com>
2024-02-19 20:35:32 -0500

    Time: 134 ms (54.00%) | Memory: 16.9 MB (67.23%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -1,13 +1,9 @@
 class Solution:
     def canPlaceFlowers(self, nums: List[int], n: int) -> bool:
-        
-        count = 0
-        f = [0] + nums + [0]
-
-        for i in range(1,len(f)-1):
-            if f[i] == 0 and f[i-1]==0 and f[i+1]==0:
-                f[i] = 1
+        for i in range(len(nums)):
+            prevOk = i==0 or nums[i-1]==0
+            nextOk = i==len(nums)-1 or nums[i+1]==0
+            if prevOk and nums[i]==0 and nextOk:
+                nums[i] = 1
                 n -= 1
-            
-        # print(f)
         return n<=0
