52f57c0b7ca2e27dafc0f90e241e02e09a3cd5e8
Raj Jada <Rajjada001@gmail.com>
2024-02-26 20:58:44 -0500

    Time: 458 ms (35.55%) | Memory: 17.1 MB (51.50%) - LeetSync

--- 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
+++ 1046-max-consecutive-ones-iii/max-consecutive-ones-iii.py
@@ -7,11 +7,9 @@ class Solution:
             if nums[end]==0:
                 count += 1
             
-            while count > k:
+            if count > k:
                 if nums[start]==0:
                     count -= 1
                 start+=1
             max_window = max(max_window, end-start+1)
         return max_window
-        
-# nums = [1,1,1,0,0,0,1,1,1,1,0], k = 2
