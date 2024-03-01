3e7eafc021052c2fec61b4f81585b5a17e89e8d0
Raj Jada <Rajjada001@gmail.com>
2024-03-01 00:52:16 -0500

    Time: 42 ms (33.19%) | Memory: 16.6 MB (45.47%) - LeetSync

--- MarchDailyChallenge/3055-maximum-odd-binary-number/maximum-odd-binary-number.py
+++ MarchDailyChallenge/3055-maximum-odd-binary-number/maximum-odd-binary-number.py
@@ -1,18 +1,8 @@
 class Solution:
     def maximumOddBinaryNumber(self, s: str) -> str:
-        countOnes = 0
-        countZero = 0
-        for ch in s:
-            if ch == '1':
-                countOnes += 1
-            else:
-                countZero += 1
-        res = ''
-        for i in range(countOnes-1):
-            res += '1'
-        for i in range(countZero):
-            res += '0'
-        
-        return res+'1'
-            
-        
+        n = sorted(s)
+        one_cnt = s.count('1')
+        zero_cnt = s.count('0')
+
+        res = '1'*(one_cnt - 1) + '0'*(zero_cnt) + '1'
+        return res
