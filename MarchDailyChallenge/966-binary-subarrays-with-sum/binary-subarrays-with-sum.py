b460ac3e87ee9fb8dfc19b16ab37e494ffceacaf
Raj Jada <Rajjada001@gmail.com>
2024-03-14 16:08:07 -0400

    Time: 219 ms (68.05%) | Memory: 20.4 MB (28.28%) - LeetSync

--- MarchDailyChallenge/966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
+++ MarchDailyChallenge/966-binary-subarrays-with-sum/binary-subarrays-with-sum.py
@@ -1,14 +1,13 @@
 class Solution:
     def numSubarraysWithSum(self, nums: List[int], goal: int) -> int:
-        def finder(target):
-            start,res,curr_sum =0,0,0
-            for end in range(len(nums)):
-                curr_sum += nums[end]
+        mydict = {0:1}
+        count = 0
+        curr = 0
+        
+        for num in nums:
+            curr += num
+            diff = curr - goal
+            count += mydict.get(diff, 0)
+            mydict[curr] = mydict.get(curr,0) + 1
 
-                while curr_sum > target and start <= end:
-                    curr_sum -= nums[start]
-                    start += 1
-                res += end-start+1
-            return res
-
-        return finder(goal) - finder(goal-1)
+        return count
